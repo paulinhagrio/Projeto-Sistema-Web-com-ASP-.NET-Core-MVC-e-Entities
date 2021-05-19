1983ea821a76228621cea3629c8f23ad0679b1f5
Paula Guimaraes <paulinhagrio@hotmail.com>
2021-05-18 22:42:42 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -22,6 +22,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _contex.Department.First();
             _contex.Add(obj);
             _contex.SaveChanges();
         }
